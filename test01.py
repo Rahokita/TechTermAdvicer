7f3bc9eea4ff0c7b33f435cfccfc46318688cc69
rahokita <rahokita7352@yahoo.co.jp>
2020-06-30 22:18:17 +0900

    Excelからシート名とセルの値を取得する

---
+++ test01.py
@@ -0,0 +1,18 @@
+import os
+import openpyxl
+
+excel_filename = 'example.xlsx'
+file_passname = os.path.dirname(__file__)
+excel_filename = file_passname + '/' + excel_filename
+
+wb = openpyxl.load_workbook(excel_filename)
+
+#シート名を出力する
+print(wb.get_sheet_names())
+#アクティブシートを取得する
+print(wb.active)
+
+#シートからセルを取得する
+WS1 = wb.get_sheet_by_name('Sheet1')
+print(WS1['B1'].value)
+print(WS1.cell(row=1,column=2).value)
