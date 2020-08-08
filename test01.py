a934e029b0274e99f553386fcbc21fe9c0a03192
rahokita <rehokita7352@yahoo.co.jp>
2020-08-08 17:28:30 +0900

    列名称と番号の変換を行う（例：A列と1）

--- test01.py
+++ test01.py
@@ -1,5 +1,6 @@
 import os
 import openpyxl
+from openpyxl.utils import get_column_letter,column_index_from_string
 
 excel_filename = 'example.xlsx'
 file_passname = os.path.dirname(__file__)
@@ -22,3 +23,7 @@ print(WS1.cell(row=1,column=2).value)
 #シートのデータサイズ（行数、列数）を取得する
 print(WS1.max_row)
 print(WS1.max_column)
+
+#列名称と番号の変換
+print(get_column_letter(2))
+print(column_index_from_string('A'))
