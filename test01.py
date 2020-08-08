8b162295d21be8f370dd2c6b2adfc57a437f1347
rahokita <rehokita7352@yahoo.co.jp>
2020-08-08 14:18:17 +0900

    シートのデータサイズ（行数、列数）を取得する処理を追加

--- test01.py
+++ test01.py
@@ -7,13 +7,18 @@ excel_filename = file_passname + '/' + excel_filename
 
 wb = openpyxl.load_workbook(excel_filename)
 
-#シート名を出力する
+#シート名を出力する(12.3.2)
 sheetnames = wb.sheetnames
 print(sheetnames)
+
 #アクティブシートを取得する
 print(wb.active)
 
-#シートからセルを取得する
+#シートからセルを取得する(12.3.3)
 WS1 = wb['Sheet1']
 print(WS1['B1'].value)
 print(WS1.cell(row=1,column=2).value)
+
+#シートのデータサイズ（行数、列数）を取得する
+print(WS1.max_row)
+print(WS1.max_column)
